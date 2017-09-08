24b6b707c60dcd09f02906c17adac76ffec55fcd
Thomas Epperson <tepperson@briskheat.com>
2017-09-08 14:51:07 -0400

    Add some conversion functions for tesla, gauss, oersted. Able to list all possible materials Add a tolerance helper function Calculate turns required to hit target inductance at a given bias Perform most of the calculations for building an inductor Add some math regarding core loss density.

--- haskell/inductors.hs
+++ haskell/inductors.hs
@@ -3,6 +3,9 @@ absolute_permeability = 4*pi/10000000
 --array helper
 makeArray x = [0..(x-1)]
 
+gaussToTesla x = x / 10000
+oerstedToAtCm x = 250 * x / pi
+
 --to convert various units to meters
 inchToMeter x = x * 0.0254
 cmToMeter x = x * 0.01
@@ -15,10 +18,50 @@ perMFTtoPerMeter x = x / 304.8
 --name, diameter, resistance/meter
 data Wire = Wire [Char] Double Double deriving (Show)
 data Shape = ToroidRect Double Double Double | EEcore Double Double deriving (Show)
-data Material = KoolMu14 | KoolMu26 | KoolMu40 | KoolMu60 | KoolMu75 | KoolMu90 | KoolMu125 | PC40
+data Material = KoolMu14 | KoolMu26 | KoolMu40 | KoolMu60 | KoolMu75 | KoolMu90 | KoolMu125 | PC40 deriving (Show, Enum, Bounded)
 data SineWave = SineWave { frequency :: Double, rms_current :: Double} deriving (Show)
 data RmsPower = RmsPower Double Double deriving (Show)
 
+allMaterial = [(minBound::Material) ..]
+
+inTolerance value expectation percent = (value >= min_val) && (value <= max_val)
+    where min_val = (1.0 - percent) * expectation
+          max_val = (1.0 + percent) * expectation
+getPercentError value expectation = (value - expectation) / expectation
+          
+findInput t guess goal tolerance
+    | abs(closeness) < tolerance = [(result, guess, closeness)]
+    | otherwise = (result, guess, closeness): (findInput t (guess / (1 + 0.5 * closeness)) goal tolerance)
+    where result = t guess
+          closeness = getPercentError result goal
+
+--things to calculate
+--number of turns to achieve inductance at peak current
+--power loss in copper
+--length of wire required
+--power loss in core
+          
+data Inductor = Inductor { 
+    ind_minL :: Double,
+    ind_mat :: Material,
+    ind_shape :: Shape,
+    ind_turns :: Double,
+    ind_w_pwr :: Double,
+    ind_wire :: Wire,
+    ind_wire_length :: Double,
+    ind_c_pwr :: Double,
+    ind_driving :: [SineWave]
+    } deriving (Show)
+
+makeInductor mat shape wire max_curr targetL toleranceL driver = 
+    Inductor achieved mat shape turns wpower wire length cpower driver
+    where turns_math = last(calcTurns shape mat wire max_curr targetL toleranceL 1)
+          achieved = snd (turns_math)
+          turns = fst (turns_math)
+          wpower = (calcWirePower shape wire turns driver)
+          cpower = 0.4321
+          length = (calcWireLength shape wire turns)
+    
 pfc_signal = [ SineWave 60 14, SineWave 40000 1.4]
 
 power_i2r (SineWave f i) r = RmsPower f (i * i * r);
@@ -68,7 +111,15 @@ me = ToroidRect (mmToMeter 102.4) (mmToMeter 165.1) (mmToMeter 31.75)
     
 calcAL relPerm area length = (absolute_permeability) * relPerm * area / length
 calcShapeAL t mat = calcAL (permeability mat) (calcMagArea t) (calcMagLength t)
-calcShapeL t mat turns current = (calcShapeAL t mat) * turns * turns * (permeability_bias mat (calcH t turns current)) 
+calcL t mat current turns = (calcShapeAL t mat) * turns * turns * (permeability_bias mat (calcH t turns current)) 
+
+calcTurns shape mat wire current targetL tolerance guess
+    | abs(closeness) < tolerance = [(guess, result)]
+    | result > targetL = [(guess, result)]
+    | guess > (calcWireMaxTurns shape wire) = [(guess * 10, 0)]
+    | otherwise = (guess, result): (calcTurns shape mat wire current targetL tolerance (guess+1))
+    where result = calcL shape mat current guess
+          closeness = getPercentError result targetL
 
 permeability :: Material -> Double
 permeability x = what x
@@ -90,25 +141,65 @@ permeability_bias x h =  what x
        what KoolMu90 = 0.01 / (0.01 + 2.698e-5 * h**1.558)
        what KoolMu125 = 0.01 / (0.01 + 6.345e-5 * h**1.462)
        what PC40 = 1.0
-   
+
+wire_power :: Shape -> Wire -> Double -> [SineWave] -> Double
+wire_power (ToroidRect a b c) d e f =  calcWirePower (ToroidRect a b c) d e f
+wire_power (EEcore a b) c d e = a * d
+       
 calcMagLength (ToroidRect id od thick) = pi * (id + od) * 0.5
 calcMagArea (ToroidRect id od thick) = (od-id) * 0.5 * thick
-calcWindingArea (ToroidRect id od thick) = pi * 0.25 * id * id
-calcWindingPercent s turns (Wire _ d _) = (pi * 0.25 * d * d * turns) / (calcWindingArea s)
-calcWireLength (ToroidRect id od thick) (Wire _ d _) turns = 2 * thick + (od - id) * 0.5
 
-calcWireLayers (ToroidRect id od thick) (Wire _ d _) = floor ((sqrt 0.3) * id * 0.5 / d)
-calcWireLayerTurns (ToroidRect id od thick) (Wire _ d _) n = floor ((id - ((n - 0.5) * d)) * pi / d)
+calcWindingArea :: Shape -> Double
+calcWindingArea (ToroidRect id od thick) = pi * 0.25 * id * id
+
+calcWindingPercent s turns (Wire _ d _) = (pi * 0.25 * d * d * turns) / (calcWindingArea s)
+
+calcWireLayers :: Shape -> Wire -> Double
+calcWireLayers (ToroidRect id od thick) (Wire _ d _) = fromIntegral(floor ((sqrt 0.3) * id * 0.5 / d))
+
+calcWireLayerTurns :: Shape -> Wire -> Double -> Double
+calcWireLayerTurns (ToroidRect id od thick) (Wire _ d _) n = fromIntegral (floor ((id - ((n - 0.5) * d)) * pi / d))
+
+calcWireMaxTurns t w = sum [(calcWireLayerTurns t w x) | x <- [1..( (calcWireLayers t w))]]
+
+calcWireLayerTurnLength :: Shape -> Wire -> Double -> Double
 calcWireLayerTurnLength (ToroidRect id od thick) (Wire _ d _) n = od-id+thick+thick+d+d+4*d*(n-1)
-calcWireMaxLength t w = sum [(calcWireLayerTurnLength t (w) x) * fromIntegral(calcWireLayerTurns t (w) x) | x <- [1..(fromIntegral (calcWireLayers t (w)))]]
-calcWireMaxResist t w = (resist w) * sum [(calcWireLayerTurnLength t (w) x) * fromIntegral(calcWireLayerTurns t (w) x) | x <- [1..(fromIntegral (calcWireLayers t (w)))]]
-calcWirePower t w s = total_power [power_i2r x (calcWireMaxResist t w) | x <- s]
+
+calcWireMaxLength t w = sum [(calcWireLayerTurnLength t (w) x) * (calcWireLayerTurns t (w) x) | x <- [1..( (calcWireLayers t (w)))]]
+calcWireMaxResist t w = (resist w) * sum [(calcWireLayerTurnLength t (w) x) * (calcWireLayerTurns t (w) x) | x <- [1..( (calcWireLayers t (w)))]]
+
+calcWireResist t w turns = (turns / (calcWireMaxTurns t w)) * (calcWireMaxResist t w)
+calcWireLength t w turns = (turns / (calcWireMaxTurns t w)) * (calcWireMaxLength t w)
+calcWirePower t w turns s = total_power [power_i2r x (calcWireResist t w turns) | x <- s]
 
 resist (Wire _ _ w) = w
 diameter (Wire _ d _) = d
 
+calcH :: Shape -> Double -> Double -> Double
 calcH (ToroidRect id od thick) turns current = 0.01 * turns * current / (pi * (id + od) * 0.5)
 
+--material and H input, output B in tesla
+calcB :: Material -> Double -> Double
+calcB mat h =  what mat
+ where what KoolMu14 =  ((1.105e-1 + 1.301e-2 * h + 6.115e-4 * h * h) / (1 + 1.386e-1 * h + 5.735e-4 * h * h)) ** 1.760
+       what KoolMu26 =  ((1.008e-1 + 1.452e-2 * h + 7.846e-4 * h * h) / (1 + 1.035e-1 * h + 7.573e-4 * h * h)) ** 1.754
+       what KoolMu40 =  ((5.180e-2 + 2.132e-2 * h + 7.941e-4 * h * h) / (1 + 8.447e-2 * h + 7.652e-4 * h * h)) ** 1.756
+       what KoolMu60 =  ((5.214e-2 + 2.299e-2 * h + 8.537e-4 * h * h) / (1 + 7.029e-2 * h + 8.183e-4 * h * h)) ** 1.658
+       what KoolMu75 =  ((4.489e-2 + 2.593e-2 * h + 7.949e-4 * h * h) / (1 + 6.463e-2 * h + 7.925e-4 * h * h)) ** 1.595
+       what KoolMu90 =  ((4.182e-2 + 2.990e-2 * h + 7.826e-4 * h * h) / (1 + 6.542e-2 * h + 7.669e-4 * h * h)) ** 1.569
+       what KoolMu125 = ((1.414e-2 + 2.850e-2 * h + 1.135e-3 * h * h) / (1 + 7.550e-2 * h + 1.088e-3 * h * h)) ** 1.274
+       what PC40 = 1.0
+
+calcCoreLossDensity mat flux freq = what mat
+ where what KoolMu14 =  1
+       what KoolMu26 =  (flux ** 1.766) * (1e-6 * 0.2546 * freq - 904.56)
+       what KoolMu40 =  (flux ** 1.766) * (1e-6 * 0.2546 * freq - 904.56)
+       what KoolMu60 =  4
+       what KoolMu75 =  5
+       what KoolMu90 =  6
+       what KoolMu125 = 7
+       what PC40 = 1.0
+       
 --[(calcWireLayerTurnLength me (magnetWire!!4) x) | x <- [1..(fromIntegral (calcWireLayers me (magnetWire!!4)))]]
 
 --sum [(calcWireLayerTurns me (magnetWire!!4) x) | x <- [1..(fromIntegral (calcWireLayers me (magnetWire!!4)))]]
